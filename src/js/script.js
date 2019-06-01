90c8f048dfc0bff0cf5d7e8681b11be69a6b4f51
Akzhar <akzharik@mail.ru>
2019-06-01 12:11:53 +0300

    change script.js layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// данные (модель)
+
 var players = [
 {
   name: '',
@@ -26,12 +28,11 @@ var winFieldsets = [
 [0,0,1,0,1,0,1,0,0]
 ]
 
-function setPlayer(i) {
-  var player = prompt('Введите имя ' + (i+1) + ' игрока (за ' + players[i].shape +')\nПо умолчанию \'Игрок ' + (i+1) + '\'', '');
-  player = (player == null) ? 'Игрок ' + (i+1) : player;
-  players[i].name = player;
+// функции вывода информации (представление)
+
+function setPlayerTitle(playerName) {
   var playerTitle = document.getElementById('players').children[i].querySelector('b');
-  playerTitle.textContent = player;
+  playerTitle.textContent = playerName;
 }
 
 function alertGameStatus(isWin, isDraw) {
@@ -44,12 +45,66 @@ function alertGameStatus(isWin, isDraw) {
   }
 }
 
-function changePlayer() {
-  playerNo = (playerNo == 0) ? 1 : 0;
+function addPlayerShape(cell) {
+  cell.classList.add(players[playerNo].cssClass);
 }
 
 function showRefreshButton() {
   refreshBtn.classList.add('show');
+}
+
+function setColorWinCells(winFieldset) {
+  for(var i = 0; i < winFieldset.length; i++) {
+    if(winFieldset[i] == 1) {
+      cells[i].classList.add('win');
+    }
+  }
+}
+
+// функции изменения модели (контроллер)
+
+function setPlayer(i) {
+  var playerName = prompt('Введите имя ' + (i+1) + ' игрока (за ' + players[i].shape +')\nПо умолчанию \'Игрок ' + (i+1) + '\'', '');
+  playerName = (playerName == null) ? 'Игрок ' + (i+1) : playerName;
+  players[i].name = playerName;
+  setPlayerTitle(playerName);
+}
+
+function changePlayer() {
+  playerNo = (playerNo == 0) ? 1 : 0;
+}
+
+function setPlayerFieldset(cell) {
+  players[playerNo].fieldset[cell.id] = 1;
+}
+
+function removeCellsClickHandler() {
+  for(var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', CellsClickHandler);
+  }
+}
+
+function checkWin() {
+  var factFieldsets = [];
+  for(var i = 0; i < winFieldsets.length; i++) {
+    factFieldsets.push([]);
+    for(var j = 0; j < winFieldsets[i].length; j++) {
+      factFieldsets[i][j] = players[playerNo].fieldset[j] * winFieldsets[i][j];
+    }
+  }
+  for(var i = 0; i < winFieldsets.length; i++) {
+    if(factFieldsets[i].join() == winFieldsets[i].join()) {
+      setColorWinCells(winFieldsets[i]);
+      return true;
+    }
+  }
+}
+
+function checkDraw() {
+  for(var i = 0; i < cells.length; i++) {
+    if(cells[i].classList.length == 0) return false;
+  }
+  return true;
 }
 
 function CellsClickHandler() {
@@ -68,73 +123,22 @@ function CellsClickHandler() {
   }
 }
 
-function setColorWinCells(winFieldset) {
-  for(var i = 0; i < winFieldset.length; i++) {
-    if(winFieldset[i] == 1) {
-      cells[i].classList.add('win');
-    }
-  }
-}
-
-function checkDraw() {
-  for(var i = 0; i < cells.length; i++) {
-    if(cells[i].classList.length == 0) return false;
-  }
-  return true;
-}
-
-function removeCellsClickHandler() {
-  for(var i = 0; i < cells.length; i++) {
-    cells[i].removeEventListener('click', CellsClickHandler);
-  }
-}
-
-function addPlayerShape(cell) {
-  cell.classList.add(players[playerNo].cssClass);
-}
-
-function setPlayerFieldset(cell) {
-  players[playerNo].fieldset[cell.id] = 1;
-}
-
-function checkWin() {
-  var factFieldsets = [];
-  for(var i = 0; i < winFieldsets.length; i++) {
-    factFieldsets.push([]);
-    for(var j = 0; j < winFieldsets[i].length; j++) {
-      factFieldsets[i][j] = players[playerNo].fieldset[j] * winFieldsets[i][j];
-    }
-  }
-
-  for(var i = 0; i < winFieldsets.length; i++) {
-    if(factFieldsets[i].join() == winFieldsets[i].join()) {
-      setColorWinCells(winFieldsets[i]);
-      return true;
-    }
-  }
-}
-
-function RefreshClickHandler() {
-  window.location.reload();
-}
-
-// *********************
-// начало программы, порядок, разделение визуального и модели
-// *********************
+// запуск игры
 
 for(var i = 0; i < players.length; i++) {
   setPlayer(i);
 }
 
 var playerNo = 0;
-
-var refreshBtn = document.getElementById('refresh');
-refreshBtn.addEventListener('click', RefreshClickHandler);
-
 var msgSpan = document.getElementById('msg');
+var refreshBtn = document.getElementById('refresh');
+var cells = document.getElementById('field').children;
+
 alertGameStatus();
 
-var cells = document.getElementById('field').children;
+refreshBtn.addEventListener('click', function() {
+  window.location.reload();
+});
 
 for(var i = 0; i < cells.length; i++) {
   cells[i].addEventListener('click', CellsClickHandler);
